dd3203158c7eba03909a33aba36b0c04509368ee
Kikoszzz <kikofcosta02@hotmail.com>
2024-11-20 10:53:24 +0000

    Create views

--- Terceira Entrega/FASE 3 SCRIPTS.sql
+++ Terceira Entrega/FASE 3 SCRIPTS.sql
@@ -8,14 +8,18 @@ ORDER BY Id_utilizador;
 -- VIEW PALESTRANTES
 CREATE VIEW view_palestrantes AS
 SELECT P.Id_utilizador, U.Nome, U.Email, U.Data_Nascimento, U.Data_Registo, P.CustoPalestrante, P.DataFicouPalestrante
-FROM Palestrantes P JOIN Utilizadores U ON P.Id_utilizador = U.Id_utilizador
+FROM Palestrantes P
+    JOIN Utilizadores U
+        ON P.Id_utilizador = U.Id_utilizador
 ORDER BY P.Id_utilizador;
 
 
 -- VIEW ADMINS
 CREATE VIEW view_administradores AS
 SELECT A.Id_utilizador, U.Nome, U.Email, U.Data_Nascimento, U.Data_Registo, A.DataAdmin
-FROM Administradores A JOIN Utilizadores U ON A.Id_utilizador = U.Id_utilizador
+FROM Administradores A
+    JOIN Utilizadores U
+        ON A.Id_utilizador = U.Id_utilizador
 ORDER BY A.Id_utilizador;
 
 
@@ -26,14 +30,53 @@ FROM Empresas
 ORDER BY Id_Empresa ASC;
 
 
--- PROCEDURE VIEW EVENTOS
-CREATE PROCEDURE proc_evento(id_emp INT)
+-- VIEW EVENTOS
+CREATE VIEW view_eventos AS
+SELECT EV.Id_Evento, EV.Nome AS EventoNome, EM.Nome AS EmpresaNome, EV.Data, EV.Local, EV.PrecoInscricao
+FROM Eventos EV
+    JOIN Empresas EM
+        ON EV.Id_Empresa = EM.Id_Empresa
+ORDER BY EV.Id_Evento ASC; 
+
+
+-- VIEW DETALHES EVENTOS
+CREATE VIEW view_eventos_detalhes AS
+SELECT EV.*, U.Nome
+FROM Eventos EV
+    JOIN Utilizadores U
+        ON EV.Id_utilizador = U.Id_utilizador
+ORDER BY EV.Id_Evento ASC;
+
+
+-- VIEW PAGAMENTOS
+CREATE VIEW view_pagamentos AS
+SELECT P.*, U.Nome
+FROM Pagamentos P
+    JOIN Inscricoes I
+        ON P.Id_Inscricao = I.Id_Inscricao
+    JOIN Utilizadores U
+        ON I.Id_utilizador = U.Id_utilizador
+ORDER BY P.Id_Pagamento ASC;
+
+
+-- VIEW DESPESAS
+CREATE VIEW view_despesas AS
+SELECT D.*, U.Nome
+FROM Despesas D
+    JOIN Utilizadores U
+        ON D.Id_utilizador = U.Id_utilizador
+ORDER BY D.Id_Despesa ASC;
+
+
+
+/*
+-- PROCEDURE VIEW DESPESAS
+CREATE PROCEDURE proc_despesas(idEvento INT, idUtilizador INT)
 LANGUAGE PLPGSQL
 AS $$
 BEGIN
-    SELECT EV.Id_Evento, EM.Nome, EV.Nome, EV.Data, EV.Local, EV.PrecoInscricao
-    FROM Eventos EV JOIN Empresas EM ON EM.Id_Empresa = EV.Id_Empresa
-    WHERE EM.Id_Empresa = id_emp
-    ORDER BY EV.Id_Evento
+RETURN QUERY
+    
 END
 $$;
+*/
